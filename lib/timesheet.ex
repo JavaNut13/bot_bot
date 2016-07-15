cbd2965c470343956eef743c1caa599608c5e9b2
Will Richardson <william.hamish@gmail.com>
2016-07-15 17:12:32 +1200

    Kind of added exrm, proper logging in to agilefant

--- lib/timesheet.ex
+++ lib/timesheet.ex
@@ -1,6 +1,7 @@
 defmodule BotBot.Timesheet do
   @elephant_timesheet "http://agilefant.cosc.canterbury.ac.nz:8080/agilefant302/generateTree.action"
   @logging_url "http://agilefant.cosc.canterbury.ac.nz:8080/agilefant302/editIteration.action?iterationId=26"
+  @login_url "http://agilefant.cosc.canterbury.ac.nz:8080/agilefant302/j_spring_security_check"
 
   @ids_and_usernames [
     {159, "bmosky"},
@@ -29,15 +30,40 @@ defmodule BotBot.Timesheet do
 
   defp get_data_for(user_id) do
     headers = %{
-      "Cookie" => "JSESSIONID=CEA647B250A98F62511F912A89D0C88B; SPRING_SECURITY_REMEMBER_ME_COOKIE=d2hyMTg6MTQ2ODEzNjA0MDM1MTphZDZhMmI2MjM3MmE3ZTUwMTNjYmZmZTI0NzY2MjQyMg",
+      "Cookie" => get_cookies,
       "Content-Type" => "application/x-www-form-urlencoded"
     }
-    data = "backlogSelectionType=0&productIds=1&projectIds=4&iterationIds=26&interval=NO_INTERVAL&startDate=&endDate=&userIds=#{user_id}"
+    data = "backlogSelectionType=0&productIds=1&projectIds=4&iterationIds=28&interval=NO_INTERVAL&startDate=&endDate=&userIds=#{user_id}"
     returned = HTTPoison.post!(@elephant_timesheet, data, headers).body
     |> Floki.find(".timesheet-header-ul .hoursum")
-    |> hd
-    {"li", _, [time]} = returned
-    time
+    
+    case returned do
+      [] -> "No time logged"
+      [{"li", _, [time]} | _] -> time
+    end
+  end
+
+  def get_cookies do
+    user = Application.get_env(:bot_bot, :agilefant_username)
+    pass = Application.get_env(:bot_bot, :agilefant_password)
+    data = "j_username=#{user}&j_password=#{pass}&_spring_security_remember_me=on"
+    headers = %{
+      "Content-Type" => "application/x-www-form-urlencoded"
+    }
+    cookies = HTTPoison.post!(@login_url, data, headers).headers
+    # Get rid of all the other headers
+    |> Stream.filter(fn
+      {"Set-Cookie", _} -> true
+      _ -> false
+    end)
+    # Only get the first bit of the cookie (ie the session ID/ whatever)
+    |> Stream.map(fn {"Set-Cookie", cookie} ->
+      [cook | _] = String.split(cookie, "; ")
+      cook
+    end)
+    # Join them all together into one cookie goodness
+    |> Enum.join("; ")
+    cookies
   end
 end
 
--- mix.exs
+++ mix.exs
@@ -2,17 +2,16 @@ defmodule BotBot.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :bot_bot,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :bot_bot,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [
       mod: {BotBot.Supervisor, []},
@@ -20,20 +19,14 @@ defmodule BotBot.Mixfile do
         :logger,
         :quantum,
         :httpoison,
-        :slack
+        :slack,
+        :poison,
+        :floki,
+        :websocket_client
       ]
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:httpoison, "~> 0.8.0"},
@@ -41,7 +34,8 @@ defmodule BotBot.Mixfile do
       {:quantum, ">= 1.7.1"},
       {:slack, "~> 0.6.0"},
       {:websocket_client, git: "https://github.com/jeremyong/websocket_client"},
-      {:floki, "~> 0.8.1"}
+      {:floki, "~> 0.8.1"},
+      {:exrm, "~> 1.0.6"}
     ]
   end
 end
